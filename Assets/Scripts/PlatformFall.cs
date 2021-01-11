f39d1d3c3b07cac646cd0df9403cca60900908f1
fialho <danidaninpce3@gmail.com>
2021-01-11 00:07:14 -0300

    Mechanic of platform fall

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour
+{
+  
+    Rigidbody2D platformRb;
+   
+   void Start()
+   {
+       platformRb = GetComponent<Rigidbody2D>();
+   }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+             Invoke("Fall",0.5f);
+        }
+       
+   
+    }
+    void Fall()
+    {
+        platformRb.gravityScale = 1;
+    }
+}
---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputs : MonoBehaviour
+{
+    private float horizontal;
+    private Vector2 newMovement;
+    private Rigidbody2D playerRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+      playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputMovement();
+       
+    }
+     void FixedUpdate()
+    {
+        playerRb.velocity = newMovement;
+      
+    }
+
+    void InputMovement()
+    {
+          horizontal = Input.GetAxisRaw("Horizontal");
+          Movement(horizontal);
+         
+    }
+    void Movement(float direction)
+    {
+          float speed = 5;
+          newMovement = new Vector2(direction *speed,playerRb.velocity.y);
+
+    }
+
+}
